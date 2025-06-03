3ba58df395bcf47c8b5fcc2e0b39741d8bc0b0dd
divya <divya@myrevealer.com>
2025-06-03 12:29:09 +0530

    Replace entire repo with local project

--- superset/initialization/__init__.py
+++ superset/initialization/__init__.py
@@ -153,6 +153,7 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
         from superset.queries.saved_queries.api import SavedQueryRestApi
         from superset.reports.api import ReportScheduleRestApi
         from superset.reports.logs.api import ReportExecutionLogRestApi
+        from superset.scheduler.api import SchedulerRestApi
         from superset.row_level_security.api import RLSRestApi
         from superset.security.api import RoleRestAPI, SecurityRestApi
         from superset.sqllab.api import SqlLabRestApi
@@ -225,6 +226,7 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
         appbuilder.add_api(SqlLabRestApi)
         appbuilder.add_api(SqlLabPermalinkRestApi)
         appbuilder.add_api(LogRestApi)
+        appbuilder.add_api(SchedulerRestApi)
         #
         # Setup regular views
         #
---
+++ superset/scheduler/api.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import logging
+from uuid import uuid4
+
+from flask import request, Response
+from flask_appbuilder.api import expose, protect, safe
+
+from superset.views.base_api import BaseSupersetApi
+
+logger = logging.getLogger(__name__)
+
+# simple in-memory store for scheduled jobs
+scheduled_jobs: dict[str, dict] = {}
+
+
+class SchedulerRestApi(BaseSupersetApi):
+    resource_name = "scheduler"
+    allow_browser_login = True
+    openapi_spec_tag = "Scheduler"
+
+    @expose("/jobs", methods=("GET",))
+    @protect()
+    @safe
+    def list_jobs(self) -> Response:
+        """Return a list of scheduled jobs"""
+        return self.response(200, result=list(scheduled_jobs.values()))
+
+    @expose("/jobs", methods=("POST",))
+    @protect()
+    @safe
+    def save_job(self) -> Response:
+        """Save a scheduler configuration"""
+        payload = request.json or {}
+        job_id = str(uuid4())
+        job = {"id": job_id, **payload}
+        scheduled_jobs[job_id] = job
+        logger.info("Saved scheduler job %s", job_id)
+        return self.response(201, result=job)
+
+    @expose("/jobs/<job_id>", methods=("DELETE",))
+    @protect()
+    @safe
+    def delete_job(self, job_id: str) -> Response:
+        """Delete a scheduled job"""
+        if job_id not in scheduled_jobs:
+            return self.response_404()
+        scheduled_jobs.pop(job_id, None)
+        logger.info("Deleted scheduler job %s", job_id)
+        return self.response(200, message="Deleted")
