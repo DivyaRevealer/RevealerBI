49791eeb524c4258fe59986a6e984e98af9abccf
divya <divya@myrevealer.com>
2025-06-05 22:04:29 +0530

    Scheduler

--- superset/scheduler/jobs.py
+++ superset/scheduler/jobs.py
@@ -30,9 +30,11 @@ def run_jobs() -> None:
     print("✅ Before logging into Superset")
     token = login_superset()
     print("✅ Logged into Superset")
+    print(token)
 
     for job in jobs.values():
         dashboard = job.get("dashboard_id") or job.get("dashboard_name")
+        print(dashboard)
         if dashboard:
             refresh_dashboard(dashboard, token)
             print("✅ Dashboard refreshed")
@@ -52,21 +54,34 @@ def start_scheduler() -> BackgroundScheduler:
         jobstores={"default": SQLAlchemyJobStore(url=jobstore_url)}
     )
     print("✅ before add_job")
+    #scheduler.add_job(
+     #   run_jobs,
+     #   "cron",
+     #   hour=18,
+     #   minute=45,
+     #   id="run_jobs",
+     #   replace_existing=True,
+    #)
+    
     scheduler.add_job(
         run_jobs,
-        "cron",
-        hour=18,
-        minute=45,
-        id="run_jobs",
+        "date",  # runs once immediately
+        run_date=None,  # means "now"
+        id="run_jobs_now",
         replace_existing=True,
     )
+    print("✅ after add_job now")
     scheduler.start()
+    print("✅ scheduler started")
     return scheduler
 
 print("✅ File loaded")
 if __name__ == "__main__":
     print("✅ inside main")
     sched = start_scheduler()
+    print("✅ after sched ")
+    print(sched)
+    
     try:
         while True:
             time.sleep(1)
